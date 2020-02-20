6b3d845b914cd52804c38cac4731771b60832832
ivan <igarciaquintela@gmail.com>
2020-02-20 19:29:09 +0100

    second commit

---
+++ HambelBandel/src/es/udc/input.java
@@ -0,0 +1,15 @@
+package es.udc;
+
+import java.io.File;
+
+public class input {
+	File archivo;
+	/** PRIMERA LINEA OBTIENE**/
+	int nLibros;
+	int nLibreria;
+	int nDias;
+	/** SEGUNDA LINEA OBTIENE SCORES QUE ALMACENAMOS EN LIBROS**/
+	Libro libro;
+	/** TERCEA 
+	
+}
