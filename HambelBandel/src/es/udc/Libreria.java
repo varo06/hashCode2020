17ba171b21ddfae3bfbbe8373adabca5a16bf8bb
Alvaro Hernandez <alvaro.hernandez1@udc.es>
2020-02-20 20:02:54 +0100

    algoritmo

--- HambelBandel/src/es/udc/Libreria.java
+++ HambelBandel/src/es/udc/Libreria.java
@@ -6,11 +6,28 @@ public class Libreria {
 	public int id;
 	public int signupdays;
 	public int booksday;
+	public int diasQueTarda;
+	public int puntuacionTotal;
 	public ArrayList<Book> books;
 	public Libreria(int idl, int signupdaysl, int booksdayl, ArrayList<Book> booksl) {
+		this.puntuacionTotal = puntuacionTotal();
+		this.diasQueTarda = diasQueTarda();
 		this.id=idl;
 		this.signupdays=signupdaysl;
 		this.booksday=booksdayl;
 		this.books= booksl;
 	}
+	public int puntuacionTotal() {
+		int scoreTotal=0;
+		for (Book b : this.books ) {
+			scoreTotal = scoreTotal + b.score;
+		}
+		return scoreTotal;
+	}
+	public int diasQueTarda() {
+		int diasTotales;
+		diasTotales = signupdays + (books.size()/booksday);
+		return diasTotales;
+	}
+	
 }
--- HambelBandel/src/es/udc/main.java
+++ HambelBandel/src/es/udc/main.java
@@ -1,7 +1,25 @@
 package es.udc;
 
-public class main {
+import java.util.ArrayList;
 
+public class main {
+	public static Libreria calcularOrdenLibreria(Libreria A,  ArrayList<Libreria> librerias, ArrayList<Libreria> libreriasOrdenada) {
+		if(librerias.isEmpty()){
+			return A;
+		}
+		else {
+			Libreria B = librerias.
+			if()
+		}
+		return B;
+	}
+	public static String algoritmo(ArrayList<Libreria> librerias, ArrayList<Book> libros, int diasLimite) {
+
+		
+		return null;
+		
+	}	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
