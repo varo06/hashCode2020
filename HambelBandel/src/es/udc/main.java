8f119af87ef2f65c5da2248fbad1d3428ba0d952
Anxo <anxoc98@gmail.com>
2020-02-20 20:33:15 +0100

    Algoritmo echo

--- HambelBandel/src/es/udc/main.java
+++ HambelBandel/src/es/udc/main.java
@@ -2,28 +2,52 @@ package es.udc;
 
 import java.util.ArrayList;
 
-
 public class main {
-	public static Libreria calcularOrdenLibreria(Libreria A,  ArrayList<Libreria> librerias, ArrayList<Libreria> libreriasOrdenada) {
+	public static void calcularOrdenLibreria(Libreria A,  ArrayList<Libreria> librerias, ArrayList<Libreria> libreriasOrdenada) {
 		if(librerias.isEmpty()){
-			return A;
+			libreriasOrdenada.add(A);
 		}
 		else {
-			Libreria B;
-		
+			int i = 1;
+			for(Libreria libreria: librerias) {
+				if(A.diasQueTarda > libreria.diasQueTarda) {
+					
+				}else {
+					if(A.diasQueTarda == libreria.diasQueTarda){
+						if(A.puntuacionTotal> libreria.puntuacionTotal) {
+							
+						}
+						else {
+							librerias.add(0,A);
+							A = librerias.remove(i);
+						}
+					}else {
+						if(A.diasQueTarda > libreria.diasQueTarda) {
+							librerias.add(0,A);
+							A = librerias.remove(i);
+						}
+					}
+				}
+
+					i++;
+				}
+			libreriasOrdenada.add(A);
+			calcularOrdenLibreria(librerias.remove(0), librerias, libreriasOrdenada);
 		}
-		return null;
 	}
+	/*public int puntuacionFinal(ArrayList<Libreria> libreriasOrdenada, int diasMax) {
+		for(Libreria)
+			
+	}*/
 	public static String algoritmo(ArrayList<Libreria> librerias, ArrayList<Book> libros, int diasLimite) {
 
+		
 		return null;
 		
 	}	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	}
 
-	
-
-}
 }
